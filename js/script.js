bfa1cbb31aab3adde4254a7169c12df45b8b5331
Rob Schuster <robert.is@icloud.com>
2023-02-06 20:30:23 -0500

    Begin setup of random image picker

--- js/script.js
+++ js/script.js
@@ -3,10 +3,29 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
+// create variables to connect to image api
 const getImage = async function () {
   const res = await fetch ("https://picsum.photos/v2/list?limit=100");
   const images = await res.json();
   console.log(images);
+
+  // select random image from the repo
+  selectRandomImage(images);
 };
 
 getImage();
+
+// randomly select one of the images in the repo
+const selectRandomImage = function(images) {
+  const randomIndex = Math.floor(Math.floor(Math.random() * images.length));
+  // console.log(`${randomIndex} is my random number`);
+
+  const randomImage = images[randomIndex];
+  console.log(randomImage);
+};
+
+// function to display the random image in the DOM
+const displayImage = function(randomImage) {
+  const author = randomImage.author;
+  console.log(`the author is ${author}`);
+}
