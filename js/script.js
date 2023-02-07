1d0725142ab9f9bb46b6e7c868497a8ecacfffd3
Rob Schuster <robert.is@icloud.com>
2023-02-06 21:23:08 -0500

    Debug completed, project complete for review

--- js/script.js
+++ js/script.js
@@ -6,36 +6,34 @@ const img = document.querySelector(".img");
 // create variables to connect to image api
 const getImage = async function () {
   const res = await fetch ("https://picsum.photos/v2/list?limit=100");
-  const images = await res.json();
-  console.log(images);
+  const images = await res.json();  // collect the images array
+  // console.log(images); //to check the function is working
 
   // select random image from the repo
-  selectRandomImage(images);  //error #3
+  selectRandomImage(images); // start random choice of an image in array function
 };
 
 // randomly select one of the images in the repo
-const selectRandomImage = function(images) {
+const selectRandomImage = function(images) {  // choose a random number between 0 - array length
   const randomIndex = Math.floor(Math.floor(Math.random() * images.length));
-  // console.log(`${randomIndex} is my random number`);
+  // console.log(`${randomIndex} is my random number`); // to check the function is working
 
-  const randomImage = images[randomIndex];
-  console.log("randomImage"); 
-  displayImage(randomImage);
+  const randomImage = images[randomIndex];  // choose from the array from the random number chosen
+  // console.log("randomImage"); //to check function is working
+  displayImage(randomImage);  // begin the display function
 };
 
 // function to display the random image in the DOM
 const displayImage = function(randomImage) {
-  const author = [randomImage.author];  // error #2
-  const imageAddress = randomImage.url;
-  authorSpan.innerText = (author);
-  img.src = imageAddress;
+  const author = randomImage.author; // path for the name of author of photo
+  const imageAddress = randomImage.download_url;  // path for randomized image
+  authorSpan.innerText = (author);  // set author text
+  img.src = imageAddress; // apply image address
   imgDiv.classList.remove("hide");
 }
 
-// event listener for button
+// listen for button
 button.addEventListener("click", function() {
   getImage();
   console.log("button pushed")
 })
-
-// coded this far and received CORB block by Chrome.
